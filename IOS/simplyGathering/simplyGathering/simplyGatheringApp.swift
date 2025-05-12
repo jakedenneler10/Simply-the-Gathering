1c6a30c97c1626d42ef030bc6bb34d12f30a5179
Jacob Hall <jacob.hall@jhnw.edu>
2025-05-12 07:24:59 -0600

    Added the IOS folder and started the XCode Project

---
+++ IOS/simplyGathering/simplyGathering/simplyGatheringApp.swift
@@ -0,0 +1,17 @@
+//
+//  simplyGatheringApp.swift
+//  simplyGathering
+//
+//  Created by Student on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct simplyGatheringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
